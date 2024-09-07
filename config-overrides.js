9efcecb658a12628885d865ed96eef664b2383e7
jiro141 <felipe.colmenaresvzla@gmail.com>
2024-09-07 08:38:55 -0400

    formulario post listo, solo practicantes, toast activo, contraseña encriptada

--- config-overrides.js
+++ config-overrides.js
@@ -5,7 +5,8 @@ module.exports = function override(config) {
     ...config.resolve.fallback,
     crypto: require.resolve('crypto-browserify'),
     stream: require.resolve('stream-browserify'),
-    buffer: require.resolve('buffer/'),  // Añade este polyfill para buffer
+    buffer: require.resolve('buffer/'),
+    vm: require.resolve('vm-browserify'),  // Añade este polyfill para vm
   };
 
   return config;
